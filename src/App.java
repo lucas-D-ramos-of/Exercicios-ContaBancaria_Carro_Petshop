3f5a218bac08e3b80da97b6447e3797ca9e5185e
lucas-D-ramos-of <lucasandraderamosof@gmail.com>
2025-04-19 11:21:22 -0300

    Criação do arquivo

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("| Menu |");
+        System.out.println("| 1- Conta Bancária |");
+        System.out.println("| 2 - Carro |");
+        System.out.println("| 3 - Petshop |");
+        int escolha = entrada.nextInt();
+
+        switch(escolha){
+
+                case 1: 
+                break; 
+
+                case 2: 
+                break;
+
+                case 3:
+                break;
+
+                default:
+
+        }
+
+    }
+}
